befa7b8bbc98be62e2ba0fef28cf93eebac65312
C272 <realrussianmanconfirmed@gmail.com>
2020-03-04 12:34:13 +0000

    Add initial data structures and visitor.

---
+++ Data Structures/IroList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iro4cli
+{
+    /// <summary>
+    /// Represents a list data structure in Iro.
+    /// </summary>
+    public class IroList : IroVariable
+    {
+        //Define the type in constructor.
+        public IroList()
+        {
+            base.Type = VariableType.Array;
+        }
+
+        //List of other variables contained within this one.
+        public List<IroVariable> Contents = new List<IroVariable>();
+    }
+}
---
+++ Data Structures/IroValues.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace iro4cli
+{
+    /// <summary>
+    /// Classes which represent single values.
+    /// </summary>
+    public class IroValue : IroVariable
+    {
+        public string Value;
+
+        public IroValue(string val)
+        {
+            base.Type = VariableType.Value;
+            Value = val;
+        }
+    }
+    
+    public class IroRegex : IroVariable
+    {
+        public Regex Value;
+
+        public IroRegex(string rawVal)
+        {
+            //Put the regex in.
+            Value = new Regex(rawVal);
+            base.Type = VariableType.Regex;
+        }
+    }
+}
