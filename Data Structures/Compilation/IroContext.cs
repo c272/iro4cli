718fcff5e976084d27f55386a12b9d11646a7525
C272 <realrussianmanconfirmed@gmail.com>
2020-03-05 20:49:49 +0000

    Add style parsing, start of context parsing.

---
+++ Data Structures/Compilation/IroContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iro4cli
+{
+    /// <summary>
+    /// Represents a single context within Iro.
+    /// </summary>
+    public class IroContext
+    {
+        public string Name;
+        public List<ContextMember> Members = new List<ContextMember>();
+
+        public IroContext(string name)
+        {
+            Name = name;
+        }
+    }
+
+    /// <summary>
+    /// A single member inside the Iro context.
+    /// </summary>
+    public class ContextMember
+    {
+        public ContextType Type;
+        public string Data;
+    }
+
+    /// <summary>
+    /// Types of context members.
+    /// </summary>
+    public enum ContextType 
+    { 
+        EOLPop,
+        EOLPush,
+        Include,
+        InlinePush,
+        Pattern,
+        Pop,
+        Push
+    }
+}
